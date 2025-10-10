aab42de86182ed18f1dae14c2990fca02edc4cc3
Jennova <Jennova.Technologies@gmail.com>
2025-10-10 17:42:41 +0200

    Switch to my repo for vendor, kernel and hardware_xiaomi
    Signed-off-by: Jennova <Jennova.Technologies@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,12 +13,12 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 rm -rf hardware/lineage/compat/Android.bp
 
 # Kernel & Vendor Sources
-git clone --depth=1 https://github.com/sayann70/vendor_xiaomi_spes -b 16 vendor/xiaomi/spes
-git clone --depth=1 https://github.com/sayann70/kernel_xiaomi_spes -b 16 kernel/xiaomi/sm6225
+git clone --depth=1 https://github.com/Jennova-technologies/vendor_xiaomi_spes -b 16-Infinity vendor/xiaomi/spes
+git clone --depth=1 https://github.com/Jennova-technologies/vendor_xiaomi_spes -b 16-Infinity kernel/xiaomi/sm6225
 
 # Hardware/Xiaomi
 rm -fr hardware/xiaomi
-git clone https://github.com/spes-development/hardware_xiaomi hardware/xiaomi
+git clone https://github.com/Jennova-technologies/hardware_xiaomi hardware/xiaomi
 rm -fr hardware/xiaomi/vibrator/effect/Android.bp
 rm -fr hardware/xiaomi/hidl/touch/Android.bp
 
